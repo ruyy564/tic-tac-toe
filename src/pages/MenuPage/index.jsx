b045e4ffd125cfdfaccc8d77922fa3a891a1871a
Klimkin <bluemars@yandex.ru>
2023-01-03 19:17:58 +0300

    адаптация

--- src/pages/MenuPage/index.jsx
+++ src/pages/MenuPage/index.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
 
 import Link from '../../components/Link';
-import Wrapper from '../../components/Wrapper';
 
 import css from './index.module.css';
 
 const MenuPage = () => {
   return (
     <div className={css.root}>
-      <Wrapper>
-        <Link to="/twoplayer">Start game</Link>
-      </Wrapper>
+      <Link to="/twoplayer">Start game</Link>
     </div>
   );
 };
